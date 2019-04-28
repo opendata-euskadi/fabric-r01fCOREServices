c856c4c446d12b502fcf3bf957b4ab4ca1132cb9
Alex <futuretelematics@gmail.com>
2019-04-29 00:23:52 +0200

    Initial commit

---
+++ src/main/java/aa14/aa14ui/r01fCOREServices/App.java
@@ -0,0 +1,13 @@
+package aa14.aa14ui.r01fCOREServices;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
