6e62bde3e699efcb90a15acc885c864e850f8005
PCI <pci@ejie.eus>
2019-08-18 00:31:44 +0200

    Removed unused imports

--- r01fNotifier/r01fNotifierClasses/src/main/java/r01f/core/notifier/Notifier.java
+++ r01fNotifier/r01fNotifierClasses/src/main/java/r01f/core/notifier/Notifier.java
@@ -1,7 +1,5 @@
 package r01f.core.notifier;
 
-import r01f.core.services.notifier.NotifierService;
-
 public interface Notifier<M> {	// message
 	/**
 	 * @return true if the notifier is enabled, false otherwise
