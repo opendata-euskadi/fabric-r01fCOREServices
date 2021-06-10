85283d7a55eef1fb277228097aacf29a9636665e
IOLABARO <i-olabarria@ejie.eus>
2021-06-10 11:29:31 +0200

    Just comment

--- r01fNotifier/r01fNotifierClasses/src/main/java/r01f/core/notifier/NotifierResponse.java
+++ r01fNotifier/r01fNotifierClasses/src/main/java/r01f/core/notifier/NotifierResponse.java
@@ -2,6 +2,29 @@ package r01f.core.notifier;
 
 import r01f.core.services.notifier.NotifierServiceResponse;
 
+/**
+ * Just an interface to return a reponse about the nofifier:
+ *
+ *  1. Just to know if notification was sucessfull.
+ *  2. Get the underlaying services response ( smms latinia,  push-firebase...)
+ *  
+ *  If there is some custom or extended implementation of Notifier just return :
+ *  		   return new  NotifierResponse() {
+									@Override
+									public boolean wasSuccessful() {									
+										return serviceResponse.wasSuccessful(); //<< reponse based on underlaying service response.
+									}						
+								
+									@Override 
+									public NotifierServiceResponse<Phone> getServiceResponse() {										
+										 return serviceResponse;
+									}} ;
+	    }
+
+ *  			
+ * @author PCI
+ *
+ */
 public interface NotifierResponse {
 	
 	public boolean wasSuccessful();	
